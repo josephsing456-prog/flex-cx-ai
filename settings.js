b0bf247bade2ba790ed06899ea45e5e5bcf529b1
josephsing456-prog <josephsing456@gmail.com>
2026-09-02 17:12:08 +0500

    Delete settings.js

--- settings.js
+++
@@ -1,4 +0,0 @@
-window.ZexSettings = {
-  getSettings: () => window.ZexStorage.get('settings', { model: 'gemini-2.5-flash', voice: true }),
-  saveSettings: (newSettings) => window.ZexStorage.set('settings', newSettings)
-};
